a961571e6acdbfcb46446491b537685ea2fe2826
balded1ams <bald3diams@gmail.com>
2024-05-15 11:22:57 +0200

    initialisation

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className = "pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32">
+
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
